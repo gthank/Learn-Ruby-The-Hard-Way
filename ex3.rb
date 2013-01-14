39c9163882c16f8df994b6a9a0982d2860ac3b24
Hank Gay <hank.gay@gmail.com>
2013-01-13 21:43:28 -0500

    1st Extra Credit for Exercise 3.

--- ex3.rb
+++ ex3.rb
@@ -1,23 +1,37 @@
+# Bog-standard I/O stuff.
 puts "I will now count my chickens:"
 
+# Use the comma operator to output multiple values (a string and a number—the result of some math) to STDOUT.
 puts "Hens", 25 + 30 / 6
+# Use the comma operator to output multiple values (a string and a number—the result of some math) to STDOUT.
 puts "Roosters", 100 - 25 * 3 % 4
 
+# Bog-standard I/O stuff.
 puts "I will now count the eggs:"
 
+# Do a bunch of math and print the results.
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# Bog-standard I/O stuff.
 puts "Is it true that 3 + 2 < 5 - 7?"
 
+# Print the results of a math inequality to STDOUT.
 puts 3 + 2 < 5 - 7
 
+# Use the comma operator to output multiple values (a string and a number—the result of some math) to STDOUT.
 puts "What is 3 + 2?", 3 + 2
+# Use the comma operator to output multiple values (a string and a number—the result of some math) to STDOUT.
 puts "What is 5 - 7?", 5 - 7
 
+# Bog-standard I/O stuff.
 puts "Oh, that's why it's false."
 
+# Bog-standard I/O stuff.
 puts "How about some more."
 
+# Use the comma operator to output multiple values (a string and a boolean—the result of some math) to STDOUT.
 puts "Is it greater?", 5 > -2
+# Use the comma operator to output multiple values (a string and a boolean—the result of some math) to STDOUT.
 puts "Is it greater or equal?", 5 >= -2
+# Use the comma operator to output multiple values (a string and a boolean—the result of some math) to STDOUT.
 puts "Is it less or equal?", 5 <= -2
