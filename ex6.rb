4320e9c6b598dcb559770ded3c3aaf7eb0f03019
Hank Gay <whg@us.ibm.com>
2013-01-21 17:00:23 -0500

    Exercise 6, Extra Credit 1.

--- ex6.rb
+++ ex6.rb
@@ -1,20 +1,34 @@
+# Store the lead-in to a bad programmer joke in a variable named 'x'.
 x = "There are #{10} types of people."
+# Store the word 'binary' in a variable named 'binary'.
 binary = "binary"
+# Store the word 'don't' in a variable named 'do_not'.
 do_not = "don't"
+# Store the punchline of said bad programmer joke in a variable named 'y'.
 y = "Those who know #{binary} and those who #{do_not}."
 
+# Print the lead-up.
 puts x
+# Print the punchline.
 puts y
 
+# Repeat the lead-up.
 puts "I said: #{x}."
+# Repeat the punchline.
 puts "I also said: '#{y}'."
 
+# Store whether we thought the joke was funny in a variable named 'hilarious'.
 hilarious = false
+# Store our opinion of the joke in a variable named 'joke_evaluation'.
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+# Tell you what we really think about that joke.
 puts joke_evaluation
 
+# Store the first half of a sentence in a variable named 'w'.
 w = "This is the left side of..."
+# Store the last half of that sentence in a variable named 'e'.
 e = "a string with a right side."
 
+# Stick 'w' and 'e' together and print out the result.
 puts w + e
